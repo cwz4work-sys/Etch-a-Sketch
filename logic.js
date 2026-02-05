b4e2b3607995a2341e33273fc6f548b45f814cde
Wei Zhe <weizhe.chong@hextar.com>
2026-02-05 09:32:11 +0800

    Implement prompt mechanism

--- logic.js
+++ logic.js
@@ -1,23 +1,45 @@
-const container = document.querySelector('.container');
-const gridDimen = container.clientHeight / 16;
+const body = document.querySelector('body');
+const button = document.createElement('button');
+button.textContent = 'Modify';
+button.style.marginBottom = '10px';
+button.style.padding = '12px 20px';
 
-for (let index = 1; index <= 16; index++) {
-    const column = `column-${index}`;
-    const div = document.createElement('div');
-    div.classList.add(column);
-    div.style.display = 'flex';
-    div.style.flexDirection = 'column';
-    div.style.width = `${gridDimen}px`;
-    container.appendChild(div);
-    for (let rowIndex = 1; rowIndex <= 16; rowIndex++) {
-        const row = `row-${rowIndex}`;
-        const rowDiv = document.createElement('div');
-        rowDiv.classList.add(row);
-        rowDiv.style.boxSizing = 'border-box';
-        rowDiv.style.height = `${gridDimen}px`;
-        rowDiv.style.border = 'black solid 0.5px';
-        rowDiv.addEventListener('mouseenter', () => rowDiv.style.backgroundColor = 'orange');
-        rowDiv.addEventListener('mouseleave', () => rowDiv.style.removeProperty('background-color'));
-        div.appendChild(rowDiv);
+function setNewGrid() {
+    const answer = prompt('How many squares per side for the grid?', '16');
+    if (answer > 100) {
+        alert('Maximum number of squares per side is 100!')
+    } else if (Number.isFinite(+answer)) {
+        drawGrid(+answer);
+    } else {
+        alert('Enter a valid number!')
     }
 }
+button.addEventListener('click', () => setNewGrid());
+
+const container = document.querySelector('.container');
+body.insertBefore(button, container);
+
+function drawGrid(newDimen) { // fix creation of new grid
+    const gridDimen = container.clientHeight / newDimen;
+    for (let index = 1; index <= newDimen; index++) {
+        const column = `column-${index}`;
+        const div = document.createElement('div');
+        div.classList.add(column);
+        div.style.display = 'flex';
+        div.style.flexDirection = 'column';
+        div.style.width = `${gridDimen}px`;
+        container.appendChild(div);
+        for (let rowIndex = 1; rowIndex <= newDimen; rowIndex++) {
+            const row = `row-${rowIndex}`;
+            const rowDiv = document.createElement('div');
+            rowDiv.classList.add(row);
+            rowDiv.style.boxSizing = 'border-box';
+            rowDiv.style.height = `${gridDimen}px`;
+            rowDiv.style.border = 'black solid 0.5px';
+            rowDiv.addEventListener('mouseenter', () => rowDiv.style.backgroundColor = 'orange');
+            div.appendChild(rowDiv);
+        }
+    }
+}
+
+drawGrid(16);
