9ba50bfe4b17cde2ae614272efa479a852a78c14
Wei Zhe <weizhe.chong@hextar.com>
2026-02-05 18:32:08 +0800

    Implement function to remove grid

--- logic.js
+++ logic.js
@@ -9,6 +9,7 @@ function setNewGrid() {
     if (answer > 100) {
         alert('Maximum number of squares per side is 100!')
     } else if (Number.isFinite(+answer)) {
+        removeGrid();
         drawGrid(+answer);
     } else {
         alert('Enter a valid number!')
@@ -19,20 +20,18 @@ button.addEventListener('click', () => setNewGrid());
 const container = document.querySelector('.container');
 body.insertBefore(button, container);
 
-function drawGrid(newDimen) { // fix creation of new grid
+function drawGrid(newDimen) { 
     const gridDimen = container.clientHeight / newDimen;
     for (let index = 1; index <= newDimen; index++) {
-        const column = `column-${index}`;
         const div = document.createElement('div');
-        div.classList.add(column);
+        div.classList.add('column');
         div.style.display = 'flex';
         div.style.flexDirection = 'column';
         div.style.width = `${gridDimen}px`;
         container.appendChild(div);
         for (let rowIndex = 1; rowIndex <= newDimen; rowIndex++) {
-            const row = `row-${rowIndex}`;
             const rowDiv = document.createElement('div');
-            rowDiv.classList.add(row);
+            rowDiv.classList.add('row');
             rowDiv.style.boxSizing = 'border-box';
             rowDiv.style.height = `${gridDimen}px`;
             rowDiv.style.border = 'black solid 0.5px';
@@ -40,6 +39,17 @@ function drawGrid(newDimen) { // fix creation of new grid
             div.appendChild(rowDiv);
         }
     }
+}
+
+function removeGrid() {
+    const rows = document.querySelectorAll('.row');
+    const columns = document.querySelectorAll('.column');
+    remove(columns);
+    remove(rows);
+}
+
+function remove(nodeList) {
+    nodeList.forEach(elem => elem.remove());
 }
 
 drawGrid(16);
